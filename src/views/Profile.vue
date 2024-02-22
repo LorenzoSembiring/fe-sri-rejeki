074eb3dc7ef649b16b6e1cbad03a15f957452922
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-02-22 18:53:19 +0700

    Feat sidebar layout
    update layout for sidebar

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,58 +1,85 @@
 <template>
-  <body>
+  <body class="body">
     <Navbar />
     <div class="container mx-4">
       <div class="row my-5">
         <!-- sidebar -->
         <div class="col-4">
-          <div class="bg-success shadow rounded">
-            <div class="mx-3">Sidebar</div>
+          <div class="p-2 bg-light shadow rounded">
+            <div class="p-3 border-bottom">
+              <img src="@/assets/default_profile_picture.jpg" alt="@/assets/default_profile_picture.jpg" class="rounded-circle" style="max-width: 15%;">
+              <div class=" d-inline mx-3 text">Malika</div>
+            </div>
+            <div class="m-3 border-bottom">
+              <div class="my-2">Biodata</div>
+              <div class="my-2">Daftar Alamat</div>
+            </div>
+            <div class="m-3 border-bottom">
+              <div class="my-2">Menunggu Pembayaran</div>
+              <div class="my-2">Riwayat Transaksi</div>
+            </div>
           </div>
         </div>
 
         <!-- profile -->
         <div class="col">
-          <div class="bg-success shadow rounded">
+          <div class="bg-light shadow rounded">
             <div class="row mx-3 py-4">
-              <div class="col-3">
-                <div class="justify-content-center picture rounded"></div>
+              <div class="col-4 border p-4 rounded shadow">
+                <!-- picture profile -->
                 <div class="d-flex justify-content-center">
-                  <button type="button" class="mt-3 btn btn-primary align-item-center">Ganti foto</button>
+                  <div class=" picture rounded"></div>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <div class="mt-2" style="font-size: smaller; color: #555;">Besar file maksimum 2Mb. Ekstensi file yang diperbolehkan: .JPG .JPEG .PNG</div>
+
+                </div>
+                <div class="d-flex justify-content-center">
+                  <button type="button" class="mt-3 btn btn-primary align-item-center">
+                    <Icon icon="mdi:image" class="" style="font-size: 24px ;" /> Ganti foto</button>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <button type="button" class="mt-3 btn btn-primary align-item-center">Ganti kata Sandi</button>                  
                 </div>
               </div>
-              <div class="col-3 text-light">
-                <div>Nama:</div>
-                <div>Username:</div>
-                <div>email:</div>
-                <div>phone:</div>
+              <div class="ms-5 col-3 text">
+                <div class="my-2">Nama:</div>
+                <div class="my-2">Username:</div>
+                <div class="my-2">email:</div>
+                <div class="my-2">phone:</div>
               </div>
-              <div class="col text-light">
-                <div>Malika</div>
-                <div>Malika 12</div>
-                <div>malika@admin.com</div>
-                <div>081234567890</div>
+              <div class="col text">
+                <div class="my-2">Malika</div>
+                <div class="my-2">Malika 12</div>
+                <div class="my-2">malika@admin.com</div>
+                <div class="my-2">081234567890</div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      
     </div>
   </body>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import { Icon } from "@iconify/vue";
 export default {
   components: {
     Navbar,
+    Icon
   },
 };
 </script>
 
-<style>
-.container {
+<style scoped>
+.body {
+  background-image: url("@/assets/bg-icon-sm.png") ;
+  background-color: rgb(255, 242, 224);
 }
-.header .name {
+.name {
   font-size: 2.5rem;
   font-family: "Plus Jakarta Sans", sans-serif;
 }
@@ -62,5 +89,9 @@ export default {
   background-image: url("@/assets/default_profile_picture.jpg");
   background-size: contain;
   background-repeat: no-repeat;
+}
+.text {
+  font-weight: 600;
+  color: #834c2c;
 }
 </style>
