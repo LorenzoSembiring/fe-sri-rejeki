a1a07a474c6e2231f2bbb26ae72fea0d0fb62bd2
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-03-16 10:45:07 +0700

    feat: Create component ProductItem
    Basic layout that include picture, product category, product name, and price

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="rounded col-2 content border">
+        <div class="d-inline">
+            <div class="img-fluid my-2" style="text-align: center">
+                <img
+                    class="justify-center"
+                    src="@/assets/default_profile_picture.jpg"
+                    alt=""
+                />
+            </div>
+        </div>
+        <div class="mx-3 kategori ">Blangkon Jogja</div>
+        <div class="mx-3 nama fs-5">Blangkon Putih Jogja</div>
+        <div class="mx-3 mb-3 harga fw-bold">50000</div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  // `setup` is a special hook dedicated for the Composition API.
+  setup() {
+    const count = ref(0)
+
+    // expose the ref to the template
+    return {
+      count
+    }
+  }
+}
+</script>
+
+<style>
+.content {
+    width: 250px;
+}
+</style>
