d48e69957b8c1c8afa451c5b230579998a2d8236
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-03-16 12:39:16 +0700

    feat: create shop page

---
+++ src/views/Shop.vue
@@ -0,0 +1,35 @@
+<template>
+  <body>
+    <Navbar/>
+      
+    <div class="text-container px-5 py-4">
+      <div class="text-tag">
+        Koleksi Kami
+      </div>
+    </div>
+    <ProductItem/>
+  </body>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import ProductItem from "@/components/ProductItem.vue";
+export default {
+  components: {
+    Navbar,
+    ProductItem
+  },
+};
+</script>
+
+<style scoped>
+
+.text-container {
+  background-color: black;
+}
+.text-tag {
+  color: #aa7d61;
+  font-size: 2.5rem;
+  font-family: "Plus Jakarta Sans", sans-serif;
+}
+</style>
