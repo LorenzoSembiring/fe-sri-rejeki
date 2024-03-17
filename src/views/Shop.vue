82e62a8fd0aee55b398e5cf8ba584ada15c7730a
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-03-17 16:00:30 +0700

    feat: rendering component based on ref

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -5,13 +5,39 @@
         <div class="text-container px-5 py-4">
             <div class="text-tag">Koleksi Kami</div>
         </div>
-        <ProductItem category="ooo" name="999" price="50000" />
+
+        <product-item class="m-2"
+            v-for="product in products"
+            :category="product.category"
+            :name="product.name"
+            :price="product.price"
+        />
     </body>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Navbar from "@/components/Navbar.vue";
 import ProductItem from "@/components/ProductItem.vue";
+
+const products = ref([
+    {
+        category: "Blangkon Jawa",
+        name: "Blangkon Batik Jogja",
+        price: "50000",
+    },
+    {
+        category: "Blangkon Jawa",
+        name: "Blangkon Batik Solo",
+        price: "55000",
+    },
+    {
+        category: "Blangkon Jawa",
+        name: "Blangkon Batik Klaten",
+        price: "60000",
+    },
+]);
+console.log(products);
 </script>
 
 <style scoped>
