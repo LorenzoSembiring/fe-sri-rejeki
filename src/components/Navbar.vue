ac1dabc5ca82908c4315dbd033a78f7d83a5b5a1
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-03-27 14:20:05 +0700

    refactor split the profile from navbar
    splitting the profile code from navbar.vue to its own component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -58,28 +58,7 @@
           </button>
         </form>
         <CartPopover class="cart-margin" />
-        <div class="border-start ps-4" v-if="token">
-          <div class="border rounded-pill" style="height: 40px; width: 150px">
-            <div
-              :class="{
-                'default-pic': !isPorfilePicExist,
-                'profile-pic': isPorfilePicExist,
-                'rounded-circle': true,
-                'mt-1': true,
-                'ms-1': true,
-              }"
-            >
-              <div class="pill-name text-nowrap ms-4 pt-1 ps-3 d-flex align-items-center">
-                Malika Malik
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="border-start ps-3 py-1" v-else>
-          <a class="login-register mx-3" href="/login">Masuk</a>
-          <a class="login-register mx-3" href="/register">Daftar</a>
-        </div>
+        <!-- <Profile/> -->
       </div>
     </div>
   </nav>
@@ -87,6 +66,7 @@
 
 <script setup>
 import CartPopover from "@/components/CartPopover.vue";
+import Profile from "@/components/Profile.vue";
 
 const token = localStorage.token ?? null;
 const isPorfilePicExist = false;
---
+++ src/components/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="border-start ps-4" v-if="token">
+          <div class="border rounded-pill" style="height: 40px; width: 150px">
+            <div
+              :class="{
+                'default-pic': !isPorfilePicExist,
+                'profile-pic': isPorfilePicExist,
+                'rounded-circle': true,
+                'mt-1': true,
+                'ms-1': true,
+              }"
+            >
+              <div class="pill-name text-nowrap ms-4 pt-1 ps-3 d-flex align-items-center">
+                Malika Malik
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="border-start ps-3 py-1" v-else>
+          <a class="login-register mx-3" href="/login">Masuk</a>
+          <a class="login-register mx-3" href="/register">Daftar</a>
+        </div>
+</template>
+
+<script setup>
+
+const token = localStorage.token ?? null;
+const isPorfilePicExist = false;
+
+const popover = ref(false);
+const popoverTrigger = ref
+
+</script>
