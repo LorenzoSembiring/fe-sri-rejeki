d3fec5eca1118fc7d7df40b88e9dc66427c1d729
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-03-29 14:57:38 +0700

    feat implement independent scrollable
    implementing independent scrollable for both col, but still unable to get rid of navbar height that making its height over 100vh, its the root cause of the main scrollbar

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,108 +1,133 @@
 <template>
-  <Navbar />
-  <div class="row">
-    <div class="col">
-      <!-- 360 button -->
-      <div class="z-1 position-absolute end-50 pe-4 pt-2">
-        <button class="btn rounded-circle bg-white border border-dark">
-          <Icon icon="iconoir:view-360" class="text-black text-opacity-75" style="font-size: 24px;"/>
-        </button>
-      </div>
-      <!-- carousel -->
-      <div id="carouselExampleIndicators" class="carousel slide z-0">
-        <div class="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+  <div>
+    <Navbar class="fixed-top" />
+  </div>
+  <div class="container-fluid">
+    <div class="row pt-5">
+      <!-- left col -->
+      <div class="col left-col" style="height: 100vh; overflow-y: auto">
+        <!-- 360 button -->
+        <div class="z-1 position-absolute end-50 pe-4 pt-2">
+          <button class="btn rounded-circle bg-white border border-dark">
+            <Icon
+              icon="iconoir:view-360"
+              class="text-black text-opacity-75"
+              style="font-size: 24px"
+            />
+          </button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              src="@/assets/default_profile_picture.jpg"
-              class="d-block w-100"
-              alt="..."
-            />
+        <!-- carousel -->
+        <div id="carouselExampleIndicators" class="carousel slide z-0">
+          <div class="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              class="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
           </div>
-          <div class="carousel-item">
-            <img
-              src="@/assets/default_profile_picture.jpg"
-              class="d-block w-100"
-              alt="..."
-            />
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src="@/assets/default_profile_picture.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="@/assets/default_profile_picture.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="@/assets/default_profile_picture.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
           </div>
-          <div class="carousel-item">
-            <img
-              src="@/assets/default_profile_picture.jpg"
-              class="d-block w-100"
-              alt="..."
-            />
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <!-- description -->
+        <div class="">
+          <div
+            class="fs-2 fw-semibold mt-5 mb-3"
+            style="font-family: 'Plus Jakarta Sans'"
+          >
+            Blangkon Jawa Tengah
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            perferendis enim praesentium recusandae obcaecati? Esse aspernatur
+            cupiditate maiores, accusamus minus dignissimos nemo quam, quaerat
+            sit minima sapiente quo quas. Dolore!
           </div>
         </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
-    <div class="col">
-      <div class="fs-2 fw-semibold mt-5 mb-1">Blangkon Jawa Tengah</div>
-      <div class="fs-3">Rp. 50.000</div>
-      <div class="mt-4 mb-2">Pilih Ukuran</div>
-      <div class="dropdown">
-        <button
-          class="btn dropdown-toggle border"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          UKURAN
-        </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">9</a></li>
-          <li><a class="dropdown-item" href="#">8</a></li>
-          <li><a class="dropdown-item" href="#">7</a></li>
-        </ul>
-      </div>
-      <div class="text-success fw-semibold my-1">TERSEDIA</div>
-      <div>
-        <Icon class="me-1" icon="la:ruler" style="font-size: 24px" />
-        <a class="text-decoration-none text-black" href="/">Panduan Ukuran</a>
-      </div>
-      <div class="mt-4">
-        <button class="btn btn-primary">
-          <Icon icon="mdi:cart" style="font-size: 20px" /> Tambah Ke Keranjang
-        </button>
+      <!-- right col -->
+      <div class="col" style="overflow-y: unset">
+        <div class="fs-2 fw-semibold mt-5 mb-1">Blangkon Jawa Tengah</div>
+        <div class="fs-3">Rp. 50.000</div>
+        <div class="mt-4 mb-2">Pilih Ukuran</div>
+        <div class="dropdown">
+          <button
+            class="btn dropdown-toggle border"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            UKURAN
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">9</a></li>
+            <li><a class="dropdown-item" href="#">8</a></li>
+            <li><a class="dropdown-item" href="#">7</a></li>
+          </ul>
+        </div>
+        <div class="text-success fw-semibold my-1">TERSEDIA</div>
+        <div>
+          <Icon class="me-1" icon="la:ruler" style="font-size: 24px" />
+          <a class="text-decoration-none text-black" href="/">Panduan Ukuran</a>
+        </div>
+        <div class="mt-4">
+          <button class="btn btn-primary">
+            <Icon icon="mdi:cart" style="font-size: 20px" /> Tambah Ke Keranjang
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -111,4 +136,14 @@
 import Navbar from "@/components/Navbar.vue";
 import { Icon } from "@iconify/vue";
 </script>
-<style></style>
+<style>
+.left-col {
+  overflow-y: scroll;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+.left-col::-webkit-scrollbar {
+  display: none;
+}
+</style>
