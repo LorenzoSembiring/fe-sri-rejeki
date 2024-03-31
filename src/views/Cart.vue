40beca3ce47c2b2fda4e5aa89b1b8244a964e811
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-03-31 15:13:48 +0700

    feat cart view basic layout
    Implementing basic layout with dummy value in cart view.

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,7 +1,74 @@
 <template>
-  <Navbar />
+  <Navbar class="fixed-top" />
+  <div class="row mt-5">
+    <div class="col-7 pt-5">
+      <div class="h1 ms-5">Keranjang</div>
+      <!-- container item -->
+      <div class="ms-5">
+        <!-- item -->
+        <div class="border d-flex rounded">
+          <div class="form-check col-1 d-flex align-items-center ms-3">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id="defaultCheck1"
+            />
+          </div>
+          <div class="col-2">
+            <img class="m-2 rounded" src="@/assets/default_profile_picture.jpg" alt="" />
+          </div>
+          <div class="col">
+            <div class="row">
+              <div class="col ms-3 mt-2 h5 fw-semibold">
+                Blangkon Jawa Tengah
+              </div>
+              <div class="d-flex justify-content-end col mt-3 me-4 fw-bold">Rp 50.000</div>
+            </div>
+              
+              <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col pt-5 summary-col">
+      <div class="h2 ms-4">Ringkasan Pesanan</div>
+      <div class="d-flex row bg-white rounded mx-3 mt-4">
+        <div class="col-8 my-3">
+          <div class="summary-text">Total Belanja</div>
+          <div class="summary-text">Biaya pengriman</div>
+          <div class="border-top mt-3 summary-text">Total</div>
+        </div>
+        <div class="col my-3">
+          <div class="summary-text">Rp. 50.000</div>
+          <div class="summary-text">Rp. 50.000</div>
+          <div class="summary-text mt-3">Rp. 100.000</div>
+        </div>
+        <div class="justify-content-center d-flex my-4">
+          <button class="btn btn-primary px-5">Bayar</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 </script>
+
+<style scoped>
+.summary-col {
+  background-color: rgb(245, 245, 245);
+  min-height: 100vh;
+}
+img {
+  width: 90px;
+}
+.summary-text {
+  font-size: 1rem;
+  color: rgb(83, 83, 83);
+  font-weight: 400;
+  font-family: "Plus Jakarta Sans", sans-serif;
+  margin-top: 1vh;
+}
+</style>
