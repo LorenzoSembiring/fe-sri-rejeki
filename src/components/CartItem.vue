e81f830045c339257faef952dc4f04650c774ae3
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-01 15:54:34 +0700

    refactor separate cart item from cart page
    separating cart item from cart page to individual component called CardItem.vue

---
+++ src/components/CartItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="border d-flex rounded">
+    <div class="form-check col-1 d-flex align-items-center ms-3">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="defaultCheck1"
+      />
+    </div>
+    <div class="col-2">
+      <img
+        class="m-2 rounded"
+        src="@/assets/default_profile_picture.jpg"
+        alt=""
+      />
+    </div>
+    <div class="col">
+      <div class="row">
+        <div class="col ms-3 mt-2 h5 fw-semibold">Blangkon Jawa Tengah</div>
+        <div class="d-flex justify-content-end col mt-3 me-4 fw-bold">
+          Rp 50.000
+        </div>
+        <div class="ms-3">Ukuran: 9</div>
+        <div class="ms-3 mt-1 border rounded" style="width: fit-content">
+          <button class="button-counter">-</button>
+          <input type="number" class="input-counter" />
+          <button class="button-counter">+</button>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  </div>
+</template>
+<style scoped>
+img {
+  width: 90px;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+textarea:focus,
+input:focus {
+  outline: none;
+}
+.button-counter {
+  border-style: none;
+  color: #aa7d61;
+  background-color: white;
+  font-weight: 800;
+  width: 30px;
+}
+.input-counter {
+  border: none;
+  width: 30px;
+  text-align: center;
+}
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -6,35 +6,7 @@
       <!-- container item -->
       <div class="ms-5">
         <!-- item -->
-        <div class="border d-flex rounded">
-          <div class="form-check col-1 d-flex align-items-center ms-3">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="defaultCheck1"
-            />
-          </div>
-          <div class="col-2">
-            <img class="m-2 rounded" src="@/assets/default_profile_picture.jpg" alt="" />
-          </div>
-          <div class="col">
-            <div class="row">
-              <div class="col ms-3 mt-2 h5 fw-semibold">
-                Blangkon Jawa Tengah
-              </div>
-              <div class="d-flex justify-content-end col mt-3 me-4 fw-bold">Rp 50.000</div>
-              <div class="ms-3">Ukuran: 9</div>
-              <div class="ms-3 mt-1 border rounded" style=" width: fit-content;">
-                <button class="button-counter">-</button>
-                <input type="number" class="input-counter">
-                <button class="button-counter">+</button>
-              </div>
-            </div>
-              
-              <div></div>
-          </div>
-        </div>
+        <CartItem class="me-4"/>
       </div>
     </div>
     <div class="col pt-5 summary-col">
@@ -42,7 +14,7 @@
       <div class="d-flex row bg-white rounded mx-3 mt-4">
         <div class="col-8 my-3">
           <div class="summary-text">Total Belanja</div>
-          <div class="summary-text">Biaya pengriman</div>
+          <div class="summary-text">Biaya pengiriman</div>
           <div class="border-top mt-3 summary-text fw-bold">Total</div>
         </div>
         <div class="col my-3">
@@ -60,6 +32,7 @@
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
+import CartItem from "@/components/CartItem.vue"
 </script>
 
 <style scoped>
@@ -67,9 +40,7 @@ import Navbar from "@/components/Navbar.vue";
   background-color: rgb(245, 245, 245);
   min-height: 100vh;
 }
-img {
-  width: 90px;
-}
+
 .summary-text {
   font-size: 1rem;
   color: rgb(83, 83, 83);
@@ -84,24 +55,5 @@ img {
   color: aliceblue;
   font-weight: 600;
 }
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-textarea:focus, input:focus{
-    outline: none;
-}
-.button-counter{
-  border-style:none;
-  color:#aa7d61;
-  background-color: white;
-  font-weight:800;
-  width: 30px;
-}
-.input-counter{
-  border:none;
-  width:30px;
-  text-align: center;
-}
+
 </style>
