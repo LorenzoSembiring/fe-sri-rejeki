3880e929ed74dfe005ff11a8b6acc89595158170
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-15 10:18:57 +0700

    feat: create ShippingItem component for modularity
    separating the shipping product item from main shipping page into a new component to make it more modular

---
+++ src/components/ShippingItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="ms-5 mt-3 p-2 border rounded">
+        <div class="row">
+          <div class="col-2">
+            <img
+              class="m-2 rounded"
+              src="@/assets/default_profile_picture.jpg"
+              alt=""
+            />
+          </div>
+          <div class="col-7 ms-3">
+            <div class="row h5">Blangkon Jawa Tengah</div>
+            <div class="row">Ukuran: 9</div>
+            <div class="row">
+              <div class="dropdown " style="padding-left: 0;">
+                <button
+                  class="btn dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >Pilih Pengiriman
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col text-end me-2 h5">1 x Rp50.000</div>
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+img {
+  width: 90px;
+}
+</style>
--- src/views/Shipping.vue
+++ src/views/Shipping.vue
@@ -15,38 +15,7 @@
           <div class="mt-1">Ganti Alamat</div>
         </a>
       </div>
-      <div class="ms-5 mt-3 p-2 border rounded">
-        <div class="row">
-          <div class="col-2">
-            <img
-              class="m-2 rounded"
-              src="@/assets/default_profile_picture.jpg"
-              alt=""
-            />
-          </div>
-          <div class="col-7 ms-3">
-            <div class="row h5">Blangkon Jawa Tengah</div>
-            <div class="row">Ukuran: 9</div>
-            <div class="row">
-              <div class="dropdown " style="padding-left: 0;">
-                <button
-                  class="btn dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >Pilih Pengiriman
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col text-end me-2 h5">1 x Rp50.000</div>
-        </div>
-      </div>
+    <ShippingItem/>
     </div>
     <div class="col pt-5 summary-col">
       <div class="h2 ms-4">Ringkasan Pesanan</div>
@@ -71,15 +40,13 @@
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
+import ShippingItem from "@/components/ShippingItem.vue";
 import { Icon } from "@iconify/vue";
 </script>
 
 <style scoped>
 .wrapper {
   margin-top: 4rem;
-}
-img {
-  width: 90px;
 }
 .dropdown-menu {
   overflow: hidden;
