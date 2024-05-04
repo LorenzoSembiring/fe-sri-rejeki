fa7e11a02035ca1bb316a671b08bc03290133640
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-04 15:28:39 +0700

    feat: fetch data product index from api

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -7,37 +7,57 @@
             <product-item class="m-2"
                 v-for="product in products"
                 :id="product.id"
-                :category="product.category"
+                :category="product.description"
                 :name="product.name"
                 :price="product.price"
-            />
+                />
+        <div>
+            {{products}}
+        </div>
     </body>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import Navbar from "@/components/Navbar.vue";
 import ProductItem from "@/components/ProductItem.vue";
+import axios from "axios";
 
-const products = ref([
-    {
-        id: 1,
-        category: "Blangkon Jawa",
-        name: "Blangkon Batik Jogja",
-        price: "50000",
-    },
-    {
-        id: 2,
-        category: "Blangkon Jawa",
-        name: "Blangkon Batik Solo",
-        price: "55000",
-    },
-    {
-        id: 3,
-        category: "Blangkon Jawa",
-        name: "Blangkon Batik Klaten",
-        price: "60000",
-    },
+const products = ref([]);
+
+const fetchProduct = async () => {
+    try {
+        const response = await axios.get(import.meta.env.VITE_API_URL + "/api/product/get");
+        products.value = response.data.data;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+    };
+
+// Fetch posts when the component is mounted
+onMounted(
+    fetchProduct
+);
+
+const prod = ref([
+{
+          id: 1,
+          category: "Blangkon Jawa",
+          name: "Blangkon Batik Jogja",
+          price: "50",
+        },
+        {
+          id: 2,
+          category: "Blangkon Jawa",
+          name: "Blangkon Batik Solo",
+          price: "55000",
+        },
+        {
+          id: 3,
+          category: "Blangkon Jawa",
+          name: "Blangkon Batik Klaten",
+          price: "60000",
+        },
 ]);
 </script>
 
