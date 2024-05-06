2368dd4e63aa9d10eda14d004909aa423ab6bc04
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-06 16:54:07 +0700

    feat: add props to product item and some adjustment

--- src/views/admin/Product.vue
+++ src/views/admin/Product.vue
@@ -1,47 +1,71 @@
 <template>
-	<div class="row">
-		<AdminSidebar />
-		<div class="col">
-			<div class="row header">
-				<div class="col h2 text-header ms-3">Daftar Produk</div>
-				<div class="col d-flex flex-row-reverse">
-					<button class="btn button-coklat"><Icon style="font-size: 25" icon="iconamoon:category-duotone" /></button>
-				</div>
-				<div class="col-4">
-					<div class="border-start ps-3">
-						<button class="btn button-coklat py-2">+ Tambah Produk</button>
-					</div>
-				</div>
-			</div>
-			<div class="row bg-light mt-4">
-				<div class="py-1 my-5 ms-4 rounded border bg-white table">
-					<AdminProductItem/>
-				</div>
-			</div>
-		</div>
-	</div>
+  <AdminSidebar>
+    <div class="col-lg-10">
+      <div class="row header">
+        <div class="col h2 text-header ms-3">Daftar Produk</div>
+        <div class="col d-flex flex-row-reverse">
+          <button class="btn button-coklat">
+            <Icon style="font-size: 25" icon="iconamoon:category-duotone" />
+          </button>
+        </div>
+        <div class="col-4">
+          <div class="border-start ps-3">
+            <button class="btn button-coklat py-2">+ Tambah Produk</button>
+          </div>
+        </div>
+      </div>
+      <div class="row bg-light mt-4" >
+        <div class="py-1 my-5 ms-4 rounded border bg-white table">
+          <AdminProductItem 
+		  v-for="product in products"
+                :id="product.id"
+                :category="product.description"
+                :name="product.name"
+                :price="product.price"
+				:status="product.status"
+		  />
+        </div>
+      </div>
+    </div>
+  </AdminSidebar>
 </template>
 
 <script setup>
 import AdminSidebar from "@/components/AdminSidebar.vue";
 import AdminProductItem from "@/components/AdminProductItem.vue";
 import { Icon } from "@iconify/vue";
+import { ref, onMounted } from 'vue';
+import axios from "axios";
+
+const products = ref([])
+
+const fetchProduct = async () => {
+	try{
+		const response = await axios.get(import.meta.env.VITE_API_URL + "/api/product/index?page=1&limit=10");
+        products.value = response.data.data;
+	} catch {
+		console.error('Error fetching products:', error);
+	}
+}
+
+onMounted(
+    fetchProduct
+);
 
 </script>
 
 <style scoped>
 .header {
-	margin-top: 10vh;
-	
+  margin-top: 10vh;
 }
 .button-coklat {
-	border-radius: 8px;
-	border-style:none;
-	background-color: #D7B19D;
-	color: rgb(255, 255, 255);
-	font-weight: 600;
+  border-radius: 8px;
+  border-style: none;
+  background-color: #d7b19d;
+  color: rgb(255, 255, 255);
+  font-weight: 600;
 }
 .text-header {
-	color: #402218;
+  color: #402218;
 }
 </style>
