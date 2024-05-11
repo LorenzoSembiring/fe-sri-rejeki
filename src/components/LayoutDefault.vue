e2f6dee1f915b9cb7af84223e33a2402fe15433d
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-11 22:37:07 +0700

    fix: admin sidebar layout

---
+++ src/components/LayoutDefault.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid-container">
+    <AdminSidebar class="sidebar"></AdminSidebar>
+    <div class="main">
+        <slot></slot>
+    </div>
+  </div>
+</template>
+<script setup>
+import AdminSidebar from "@/components/AdminSidebar.vue";
+</script>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-areas:
+    "sidebar main"
+    "sidebar main";
+    grid-template-columns: 250px auto;
+}
+.main {
+    grid-area: main;
+}
+.sidebar {
+    grid-area: sidebar;
+}
+</style>
