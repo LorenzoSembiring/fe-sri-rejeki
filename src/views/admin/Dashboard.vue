b47f6b31fddb1976fad716dfa3840d08484bdc3c
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-13 08:42:33 +0700

    feat: imlementing layoutdefault
    implementing layout default instead of using a col for admin sidebar

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,36 +1,20 @@
 <template>
-  <AdminSidebar>
-  <div class="row">
-    <div class="col pt-5">
-      <div class="h2">Halo, Admin</div>
+  <LayoutDefault>
+    <div class="header">
+      <div class="col h2 text-header px-4">Halo, Admin</div>
     </div>
-  </div>
-  </AdminSidebar>
+  </LayoutDefault>
 </template>
 
 <script setup>
-import AdminSidebar from "@/components/AdminSidebar.vue";
+import LayoutDefault from "@/components/LayoutDefault.vue";
 </script>
 
 <style scoped>
-.sidebar {
-  height: 100vh;
-  overflow-y: auto;
-  background: linear-gradient(180deg, rgb(180, 114, 72) 0%, rgba(170,125,97,1) 100%);
-
+.header {
+  margin-top: 10vh;
 }
-.col-1 {
-  flex: 0 0 auto;
-  width: 13%;
-}
-.sidebar-button {
-  text-decoration: none;
-  font-weight: 700;
-}
-.picture {
-  margin-top: 3px;
-  height: 40px;
-  width: 40px;
-  background-image: url("@/assets/default_profile_picture.jpg");
+.text-header {
+  color: #402218;
 }
 </style>
