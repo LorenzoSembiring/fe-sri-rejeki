b1a79532ad96924afdbaa6d954088152f15b5681
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-05-27 06:16:57 +0700

    feat: call store api

--- src/views/admin/AddCategory.vue
+++ src/views/admin/AddCategory.vue
@@ -8,12 +8,12 @@
 				<div class="d-flex align-items-center row fw-semibold p-2" style="font-size: 1rem;" >
 					<div class="my-4 fw-semibold text-grey d-flex">
             <div class="col-4 p-0">Nama Kategori</div>
-            <input type="text" class="form-control" id="name" />
+            <input type="text" class="form-control" id="name" v-model="category"/>
           </div>
 				</div>
         <div class="d-flex justify-content-end">
           <button class="button-putih py-2 px-5 mx-4" @click="back">Batal</button>
-          <button class="button-coklat py-2 px-5">Simpan</button>
+          <button class="button-coklat py-2 px-5" @click="submit" >Simpan</button>
         </div>
 			</div>
     </div>
@@ -23,9 +23,38 @@
 <script setup>
 import LayoutDefault from "@/components/LayoutDefault.vue";
 import router from "../../router/index.js";
+import { ref } from "vue";
+import axios from "axios";
+
+const category = ref("")
+const token = localStorage.getItem("token");
 
 function back() {
   router.push("/admin/category");
+}
+
+async function submit() {
+  try {
+    const formData = new FormData();
+    formData.append("name", category.value);
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/category/store/`,
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (response.status == 201) {
+      router.push("/admin/category");
+    } else {
+      // show modal
+    }
+  } catch (error) {
+    console.error("Error updating category:", error);
+  }
 }
 </script>
 
