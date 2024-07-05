de209234244d7582cdd7c1095d3e817da804ce15
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-05 14:54:09 +0700

    feat: border only showing on hover

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="rounded col-2 d-inline-block content border">
-        <RouterLink :to="'/product/' + id">    
-            <div class="d-inline">
-                <div class="img-fluid my-2" style="text-align: center">
-                    <img
-                        class="justify-center"
-                        src="@/assets/default_profile_picture.jpg"
-                        alt=""
-                    />
-                </div>
-            </div>
-        </RouterLink>
-        <RouterLink :to="'/shop/category/' + categoryId">
-            <div class="mx-4 text-dark kategori">{{ category }}</div>
-        </RouterLink>
-        <RouterLink :to="'/product/' + id">
-            <div class="mx-4 text-dark fs-5">{{ name }}</div>
-        </RouterLink >
-        <div class="mx-4 mb-3 harga fw-bold">{{ formatToIDR(price) }}</div>
-    </div>
+  <div
+    :class="['col-2', 'd-inline-block', 'content', { border: isHovered }]"
+    @mouseover="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <RouterLink :to="'/product/' + id">
+      <div class="d-inline">
+        <div class="img-fluid my-2" style="text-align: center">
+          <img
+            class="justify-center"
+            src="@/assets/default_profile_picture.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </RouterLink>
+    <RouterLink :to="'/shop/category/' + categoryId">
+      <div class="mx-4 text-dark kategori">{{ category }}</div>
+    </RouterLink>
+    <RouterLink :to="'/product/' + id">
+      <div class="mx-4 text-dark fs-5">{{ name }}</div>
+    </RouterLink>
+    <div class="mx-4 mb-3 harga fw-bold">{{ formatToIDR(price) }}</div>
+  </div>
 </template>
 
 <script setup>
 import router from "../router/index.js";
+import { ref } from 'vue';
 
 defineProps({
     id: Number,
@@ -32,24 +37,25 @@ defineProps({
     price: Number,
 });
 
+const isHovered = ref(false);
+
 function toCategory(id) {
-    router.push("/shop/category/" + id)
+  router.push("/shop/category/" + id);
 }
 function formatToIDR(number) {
-    return number.toLocaleString("id-ID", {
-        style: "currency",
-        currency: "IDR",
-    });
+  return number.toLocaleString("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
 }
 </script>
 
 <style>
 .content {
-    width: 250px;
+  width: 250px;
 }
 
 a {
-    text-decoration: none;
+  text-decoration: none;
 }
-
 </style>
