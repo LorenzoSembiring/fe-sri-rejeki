b290631606e05d384dfd151d397378bcad09d1a8
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-12 16:38:50 +0700

    feat: address card

--- src/views/ProfileAddress.vue
+++ src/views/ProfileAddress.vue
@@ -1,157 +1,152 @@
 <template>
-    <Navbar class="fixed-top" />
-    <div class="row mt-5 pt-5">
-      <div class="col-3">
-        <div class="ms-5 border rounded px-3">
-          <div class="px-2 pe-3 my-3 border-bottom">
-            <div
-              class="d-flex ms-1 border-profile rounded-pill row mt-auto mb-2"
-              style="height: 9vh; width: 100%"
-            >
-              <div class="col-3 align-items-center p-1">
-                <img
-                  class="rounded-circle"
-                  style="height: 7vh; width: 7vh"
-                  src="@/assets/default_profile_picture.jpg"
-                />
-              </div>
-              <div
-                class="text-brown fw-semibold d-flex align-items-center col"
-                style="height: 100%"
-              >
-                {{ username }}
-              </div>
-            </div>
-          </div>
-          <div class="px-3 py-2 border-bottom">
-            <div class="py-2 sidebar-list">Biodata</div>
-            <div class="py-2 sidebar-list">Daftar Alamat</div>
-          </div>
-          <div class="px-3 pt-2 pb-4">
-            <div class="py-2 sidebar-list">Menunggu Pembayaran</div>
-            <div class="py-2 sidebar-list">Riwayat Transaksi</div>
-          </div>
-        </div>
-      </div>
-      <div class="col pt-4 summary-col">
-        <div class="row">
-          <div class="col-4 border-end">
-            <div class="d-flex ps-5">
-            <Icon
-              class="text-secondary"
-              icon="mdi:account-outline"
-              style="font-size: 24px"
-            />
-            <p class="text-secondary fw-bold">{{ username }}</p>
-          </div>
-            <div class="d-flex justify-content-center">
+  <Navbar class="fixed-top" />
+  <div class="row mt-5 pt-5">
+    <div class="col-3">
+      <div class="ms-5 border rounded px-3">
+        <div class="px-2 pe-3 my-3 border-bottom">
+          <div
+            class="d-flex ms-1 border-profile rounded-pill row mt-auto mb-2"
+            style="height: 9vh; width: 100%"
+          >
+            <div class="col-3 align-items-center p-1">
               <img
+                class="rounded-circle"
+                style="height: 7vh; width: 7vh"
                 src="@/assets/default_profile_picture.jpg"
-                alt="@/assets/default_profile_picture.jpg"
-                style="max-width: 100%"
               />
             </div>
-            <div class="px-5 mx-3" style="font-size: smaller; color: #555">
-              Besar file maksimum 2Mb. Ekstensi file yang diperbolehkan: .JPG .JPEG .PNG
-            </div>
-            <div class="mt-3 d-flex justify-content-center">
-              <button class="btn button-foto border">
-                <Icon icon="mdi:image" class="" style="font-size: 24px" />Ubah
-                foto
-              </button>
-            </div>
-          </div>
-          <div class="col ps-4">
-            <div class="row">
-              <div class="fw-semibold">Biodata Diri</div>
-              <div class="col-4">
-                <div class="my-2">Nama</div>
-                <div class="my-2">Username</div>
-                <div class="my-2">Email</div>
-                <div class="my-2">Nomor Telpon</div>
-              </div>
-              <div class="col">
-                <div class="my-2">{{users.first_name}}</div>
-                <div class="my-2">{{users.username}}</div>
-                <div class="my-2">{{users.email}}</div>
-                <div class="my-2">{{users.phone}}</div>
-              </div>
+            <div
+              class="text-brown fw-semibold d-flex align-items-center col"
+              style="height: 100%"
+            >
+              {{ username }}
             </div>
           </div>
         </div>
-        <!-- <div class="d-flex bg-white">
-          <div>
+        <div class="px-3 py-2 border-bottom">
+          <div @click="router.push('/profile')" class="py-2 sidebar-list">
+            Biodata
+          </div>
+          <div class="py-2 sidebar-list">Daftar Alamat</div>
+        </div>
+        <div class="px-3 pt-2 pb-4">
+          <div class="py-2 sidebar-list">Menunggu Pembayaran</div>
+          <div class="py-2 sidebar-list">Riwayat Transaksi</div>
+        </div>
+      </div>
+    </div>
+    <div class="col pt-4 summary-col">
+      <div class="row">
+        <div class="col-10">
+          <div class="d-flex ps-5 pb-3">
             <Icon
               class="text-secondary"
               icon="mdi:account-outline"
               style="font-size: 24px"
             />
             <p class="text-secondary fw-bold">{{ username }}</p>
+            <div class="ms-auto">
+              <button class="btn button border">+ Tambah Alamat</button>
+            </div>
           </div>
-          <div class="bg-primary">
-            <div>a</div>
+          <div class="col">
+            <div class="ms-5 mb-3 p-4 border rounded card">
+              <div class="fw-semibold text-secondary">ALAMAT PENGIRIMAN</div>
+              <div class="fw-bold my-1">
+                <Icon icon="gg:pin" />
+                Rumah • Malik
+              </div>
+              <div>
+                Jl. Ahmad Yani No. 123, Kelurahan Cempaka Putih, Kota Jakarta
+                Pusat, DKI Jakarta, 081123456789
+              </div>
+            </div>
+            <div class="ms-5 p-4 border rounded card-selected">
+              <div class="fw-semibold text-secondary">ALAMAT PENGIRIMAN</div>
+              <div class="fw-bold my-1">
+                <Icon icon="gg:pin" />
+                Rumah • Malik
+              </div>
+              <div>
+                Jl. Ahmad Yani No. 123, Kelurahan Cempaka Putih, Kota Jakarta
+                Pusat, DKI Jakarta, 081123456789
+              </div>
+            </div>
           </div>
-        </div> -->
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import Navbar from "@/components/Navbar.vue";
-  import { Icon } from "@iconify/vue";
-  import { onMounted, ref } from 'vue';
-  import axios from "axios";
-  
-  const token = localStorage.getItem("token");
-  const users = ref([""])
-  const username = localStorage.name ?? null;
-  
-  async function fetchUser() {
-    try {
-      const response = await axios.get(
-        import.meta.env.VITE_API_URL + "/api/auth/get/",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      users.value = response.data.data;
-      console.log(users.value);
-    } catch (error) {
-      console.log("Error fetching user:", error);
-    }
+  </div>
+</template>
+
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import { Icon } from "@iconify/vue";
+import { ref } from "vue";
+import router from "../router/index.js";
+import axios from "axios";
+
+const token = localStorage.getItem("token");
+const users = ref([""]);
+const username = localStorage.name ?? null;
+
+async function fetchUser() {
+  try {
+    const response = await axios.get(
+      import.meta.env.VITE_API_URL + "/api/auth/get/",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    users.value = response.data.data;
+    console.log(users.value);
+  } catch (error) {
+    console.log("Error fetching user:", error);
   }
-  fetchUser()
-  // onMounted()
-  </script>
-  
-  <style scoped>
-  .sidebar-list {
-    cursor: pointer;
-    padding-left: 5px;
-    padding-right: 5px;
-  }
-  .sidebar-list:hover {
-    background-color: #f7f5f0;
-    border-radius: 1px;
-    font-weight: 500;
-  }
-  .button-foto {
-    align-items: center;
-    background-color: white;
-    color: rgb(105, 75, 58);
-    cursor: pointer;
-    font-family: "Plus Jakarta Sans", sans-serif;
-    font-size: 16px;
-    font-weight: 600;
-    letter-spacing: 1px;
-    max-width: 100%;
-  }
-  .button-foto:hover {
-    background-color: #fffaf8;
-    color: #694b3a;
-    transform: scale(0.98);
-  }
-  </style>
-  
+}
+fetchUser();
+</script>
+
+<style scoped>
+.sidebar-list {
+  cursor: pointer;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+.sidebar-list:hover {
+  background-color: #f7f5f0;
+  border-radius: 1px;
+  font-weight: 500;
+}
+.button {
+  align-items: center;
+  background-color: white;
+  color: rgb(105, 75, 58);
+  cursor: pointer;
+  font-family: "Plus Jakarta Sans", sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  max-width: 100%;
+}
+.button:hover {
+  background-color: #fffaf8;
+  color: #694b3a;
+  transform: scale(0.98);
+}
+.card {
+  background-image: url('@/assets/kratos.png');
+  background-repeat: no-repeat;
+  background-position: bottom right;
+}
+.card-selected {
+  background-image: url('@/assets/kratos-success.png');
+  background-color: #f9fffa;
+  border-color: #c0ffc8 !important;
+  border-width: 2px !important;
+  background-repeat: no-repeat;
+  background-position: bottom right;
+}
+</style>
