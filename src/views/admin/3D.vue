7ccd2f15efa33222eac3a2f5f1d748d4ad6bf371
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-07-24 19:30:47 +0700

    feat: delete mesh

--- src/views/admin/3D.vue
+++ src/views/admin/3D.vue
@@ -13,50 +13,117 @@
         <div class="col-3" v-for="item in mesh">
           <div class="d-inline-block border rounded">
             <div class="m-2 justify-content-center d-flex">
-              <View3D width="200" height="200" :mesh="item.path" :texture="texture"></View3D>
+              <View3D
+                width="200"
+                height="200"
+                :mesh="item.path"
+                :texture="texture"
+              ></View3D>
             </div>
             <div class="my-2 text-center fw-semibold">{{ item.name }}</div>
+            <div class="d-flex justify-content-center">
+              <div class="pe-3">
+                <Icon icon="material-symbols:edit" class="text-primary fs-4"/>
+              </div>
+              <div @click="shareData(item.id, item.name)" style="cursor: pointer;"  data-bs-toggle="modal" data-bs-target="#deleteModal">
+                <Icon icon="iconamoon:trash"  class="text-danger fs-4"/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div></div>
-  </LayoutDefault>
+    <!-- modal -->
+    <div
+      class="modal fade"
+      id="deleteModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi penghapusan</h1>
+          </div>
+          <div class="modal-body">Anda yakin ingin menghapus 3D objek {{ dataName }}</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn button-putih border"
+              data-bs-dismiss="modal"
+            >
+              Tidak
+            </button>
+            <button type="button" class="btn button-merah" @click="deleteMesh(dataID)">Ya, Hapus</button>
+          </div>
+        </div>
+      </div>
+    </div>  </LayoutDefault>
 </template>
 
 <script setup>
 import LayoutDefault from "@/components/LayoutDefault.vue";
 import View3D from "@/components/View3D.vue";
 import router from "../../router/index.js";
-import axios from 'axios'
-import { ref } from 'vue'
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { Icon } from "@iconify/vue";
 
 const token = localStorage.getItem("token");
-const mesh = ref([])
-const texture = "/uploads/texture/checker-default.png"
+const mesh = ref([]);
+const dataID = ref("")
+const dataName = ref("")
+const texture = "/uploads/texture/checker-default.png";
 
 function routeToAdd3DView() {
   router.push("/admin/add-3d");
 }
 
-async function fetchMesh() {
-  try {
-      const response = await axios.get(
-        import.meta.env.VITE_API_URL + "/api/mesh/index",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      mesh.value = response.data.data;
-      console.log(mesh.value)
-    } catch (error) {
-      console.error("Error fetching mesh:", error);
-    }
+function shareData(id, name) {
+  dataID.value = id
+  dataName.value = name
 }
 
-fetchMesh();
+async function fetchMesh() {
+  try {
+    const response = await axios.get(
+      import.meta.env.VITE_API_URL + "/api/mesh/index",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    mesh.value = response.data.data;
+    console.log(mesh.value);
+  } catch (error) {
+    console.error("Error fetching mesh:", error);
+  }
+}
+async function deleteMesh(id) {
+  try {
+    const response = await axios.delete(
+      `${import.meta.env.VITE_API_URL}/api/mesh/delete/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      }
+    );
+    if (response.status == 200) {
+      location.reload()
+    } else {
+      
+    }
+  } catch (error) {
+    
+  }
+}
+onMounted(async () => {
+  fetchMesh()
+});
 </script>
 
 <style scoped>
@@ -69,8 +136,28 @@ fetchMesh();
 .button-coklat {
   border-radius: 8px;
   border-style: none;
-  background-color: #A77155;
+  background-color: #a77155;
   color: rgb(255, 255, 255);
   font-weight: 600;
+}
+.button-putih {
+  border-radius: 8px;
+  border-style: none;
+  background-color: #ffffff;
+  color: rgb(53, 53, 53);
+  font-weight: 600;
+}
+.button-putih:hover {
+  background-color: #ececec;
+}
+.button-merah {
+  border-radius: 8px;
+  border-style: none;
+  background-color: #d14747;
+  color: rgb(255, 255, 255);
+  font-weight: 600;
+}
+.button-merah:hover {
+  background-color: #be2020;
 }
 </style>
