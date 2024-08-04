5b5f10fd58236f19d7bf15b523490dd91e0cdab6
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-08-04 16:50:39 +0700

    feat: change layout of 3d view

--- src/views/View3D.vue
+++ src/views/View3D.vue
@@ -1,17 +1,49 @@
 <template>
-  <View3D :height="500" :width="1000" :mesh="meshData.path" :texture="meshData.texture" v-if="meshData" />
+  <div>
+    <div class="p-2">
+      <a :href="href"> <Icon icon="material-symbols:arrow-back" /> Kembali </a>
+    </div>
+    <div class="d-flex justify-content-center">
+      <View3D
+        :height="500"
+        :width="1000"
+        :mesh="meshData.path"
+        :texture="meshData.texture"
+        v-if="meshData"
+        :style="{ cursor: isGrabbing ? 'grabbing' : 'grab' }"
+        @mousedown="startGrabbing"
+        @mouseup="stopGrabbing"
+        @mouseleave="stopGrabbing"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import View3D from "@/components/View3D.vue";
 import { ref, onMounted, watch } from "vue";
 import axios from "axios";
+import { Icon } from "@iconify/vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 
 const id = ref("");
 const meshData = ref(null);
+
+const isGrabbing = ref(false);
+const href = ref('');
+
+const updateHref = (id) => {
+  href.value = `/product/${id}`;
+};
+const startGrabbing = () => {
+  isGrabbing.value = true;
+};
+
+const stopGrabbing = () => {
+  isGrabbing.value = false;
+};
 
 async function fetchMesh() {
   try {
@@ -27,5 +59,6 @@ async function fetchMesh() {
 onMounted(async () => {
   id.value = route.params.id;
   await fetchMesh();
+  updateHref(id.value)
 });
 </script>
