95ab6cecc5e2eb79c3ca71524d89b43afb11fa5a
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-11-04 16:34:03 +0700

    refactor: prettier

--- src/components/AdminSidebar.vue
+++ src/components/AdminSidebar.vue
@@ -1,41 +1,80 @@
 <template>
-  <div class="sidebar pt-3 sticky-top border-end d-flex flex-column" style="height: 100vh">
+  <div
+    class="sidebar pt-3 sticky-top border-end d-flex flex-column"
+    style="height: 100vh"
+  >
     <div class="d-flex justify-content-center my-4">
       <img style="height: 5vh; width: auto" src="@/assets/logo-cropped.png" />
     </div>
     <div class="flex-grow-1 mx-4">
-      <div class="border-top py-2 my-2" :class="{ 'bg-brown rounded': route == 'dashboard' }">
-        <a class="ms-3 text-brown fw-semibold sidebar-button" :class="{ 'text-white': route == 'dashboard' }" href="/admin/dashboard"
-          ><Icon class="" style="font-size: 20" icon="teenyicons:home-outline" />
+      <div
+        class="border-top py-2 my-2"
+        :class="{ 'bg-brown rounded': route == 'dashboard' }"
+      >
+        <a
+          class="ms-3 text-brown fw-semibold sidebar-button"
+          :class="{ 'text-white': route == 'dashboard' }"
+          href="/admin/dashboard"
+          ><Icon
+            class=""
+            style="font-size: 20"
+            icon="teenyicons:home-outline"
+          />
           Dashboard
         </a>
       </div>
-      <div class="py-2 my-2" :class="{ 'bg-brown rounded': route == 'statistic' }">
-        <a class="ms-3 text-brown fw-semibold sidebar-button" :class="{ 'text-white': route == 'statistic' }" href="/admin/statistic"
+      <div
+        class="py-2 my-2"
+        :class="{ 'bg-brown rounded': route == 'statistic' }"
+      >
+        <a
+          class="ms-3 text-brown fw-semibold sidebar-button"
+          :class="{ 'text-white': route == 'statistic' }"
+          href="/admin/statistic"
           ><Icon style="font-size: 20" icon="uil:chart-line" />
           Statistik
         </a>
       </div>
-      <div class="py-2 my-2" :class="{ 'bg-brown rounded': route == 'product' }">
-        <a class="ms-3 text-brown fw-semibold sidebar-button" :class="{ 'text-white': route == 'product' }" href="/admin/product"
+      <div
+        class="py-2 my-2"
+        :class="{ 'bg-brown rounded': route == 'product' }"
+      >
+        <a
+          class="ms-3 text-brown fw-semibold sidebar-button"
+          :class="{ 'text-white': route == 'product' }"
+          href="/admin/product"
           ><Icon style="font-size: 20" icon="mynaui:package" />
           Produk
         </a>
       </div>
       <div class="py-2 my-2" :class="{ 'bg-brown rounded': route == 'order' }">
-        <a class="ms-3 text-brown fw-semibold sidebar-button" :class="{ 'text-white': route == 'order' }" href="/admin/order">
+        <a
+          class="ms-3 text-brown fw-semibold sidebar-button"
+          :class="{ 'text-white': route == 'order' }"
+          href="/admin/order"
+        >
           <Icon style="font-size: 20" icon="mdi:cart" />
           Pesanan
         </a>
       </div>
-      <div class="py-2 my-2" :class="{ 'bg-brown rounded': route == 'category' }">
-        <a class="ms-3 text-brown fw-semibold sidebar-button" :class="{ 'text-white': route == 'category' }" href="/admin/category">
+      <div
+        class="py-2 my-2"
+        :class="{ 'bg-brown rounded': route == 'category' }"
+      >
+        <a
+          class="ms-3 text-brown fw-semibold sidebar-button"
+          :class="{ 'text-white': route == 'category' }"
+          href="/admin/category"
+        >
           <Icon style="font-size: 20" icon="fluent:apps-list-24-filled" />
           Kategori
         </a>
       </div>
       <div class="py-2 my-2" :class="{ 'bg-brown rounded': route == 'd3' }">
-        <a class="ms-3 text-brown fw-semibold sidebar-button" :class="{ 'text-white': route == 'd3' }" href="/admin/3d"
+        <a
+          class="ms-3 text-brown fw-semibold sidebar-button"
+          :class="{ 'text-white': route == 'd3' }"
+          href="/admin/3d"
           ><Icon style="font-size: 20" icon="iconoir:3d-select-solid" />
           3D
         </a>
@@ -53,9 +92,16 @@
         style="height: 9vh; width: 100%"
       >
         <div class="col-3 align-items-center p-1">
-          <img class="rounded-circle" style="height: 7vh; width: 7vh;" :src="profilePicture">
+          <img
+            class="rounded-circle"
+            style="height: 7vh; width: 7vh"
+            :src="profilePicture"
+          />
         </div>
-        <div class="text-brown fw-semibold d-flex align-items-center col" style="height: 100%">
+        <div
+          class="text-brown fw-semibold d-flex align-items-center col"
+          style="height: 100%"
+        >
           {{ username }}
         </div>
       </div>
@@ -65,13 +111,13 @@
 
 <script setup>
 import { Icon } from "@iconify/vue";
-import axios from 'axios';
+import axios from "axios";
 import { ref, computed } from "vue";
 import router from "../router/index.js";
 import defaultProfilePicture from "@/assets/default_profile_picture.jpg";
 
 defineProps({
-  route: String
+  route: String,
 });
 
 const username = localStorage.name ?? null;
@@ -129,10 +175,10 @@ fetchUserPicture();
   background-color: #f0eae8;
 }
 .bg-brown {
-  background-color: #865439
+  background-color: #865439;
 }
 .text-brown {
-  color: #865439
+  color: #865439;
 }
 .picture {
   width: 20px;
@@ -144,7 +190,7 @@ fetchUserPicture();
 .border-profile {
   border-style: solid;
   border-width: 1px;
-  border-color: #865439
+  border-color: #865439;
 }
 a {
   text-decoration: none;
